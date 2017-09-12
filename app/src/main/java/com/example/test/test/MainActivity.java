749ef091d6bc128028db8c0bf776aa8dabd69c86
Mathias Hagen <mathias@local-host.no>
2017-09-12 13:55:02 +0200

    Initial commit

---
+++ app/src/main/java/com/example/test/test/MainActivity.java
@@ -0,0 +1,107 @@
+package com.example.test.test;
+
+import android.os.Handler;
+import android.os.SystemClock;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+    TextView textView;
+    Button start, stop, reset;
+    long MillisecondTime, StartTime, TimeBuff, UpdateTime = 0L;
+    Handler handler;
+    int Seconds, Minutes, MilliSeconds;
+    ListView listView;
+    String[] ListElements = new String[] { };
+    List<String> ListElementsArrayList;
+    ArrayAdapter<String> adapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        textView = (TextView)findViewById(R.id.textView);
+        start = (Button)findViewById(R.id.button);
+        stop = (Button)findViewById(R.id.button2);
+        reset = (Button)findViewById(R.id.button3);
+        listView = (ListView)findViewById(R.id.listview1);
+
+        handler = new Handler();
+
+        ListElementsArrayList = new ArrayList<String>(Arrays.asList(ListElements));
+
+        adapter = new ArrayAdapter<String>(MainActivity.this,
+                android.R.layout.simple_list_item_1,
+                ListElementsArrayList
+        );
+
+        listView.setAdapter(adapter);
+
+        start.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                StartTime = SystemClock.uptimeMillis();
+                handler.postDelayed(runnable, 0);
+
+                reset.setEnabled(false);
+            }
+        });
+
+        stop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                TimeBuff += MillisecondTime;
+
+                handler.removeCallbacks(runnable);
+
+                reset.setEnabled(true);
+            }
+        });
+
+
+        reset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                MillisecondTime = 0L ;
+                StartTime = 0L ;
+                TimeBuff = 0L ;
+                UpdateTime = 0L ;
+                Seconds = 0 ;
+                Minutes = 0 ;
+                MilliSeconds = 0 ;
+
+                textView.setText("0:00");
+
+                ListElementsArrayList.clear();
+
+                adapter.notifyDataSetChanged();
+            }
+        });
+    }
+
+    public Runnable runnable = new Runnable() {
+        public void run() {
+            MillisecondTime = SystemClock.uptimeMillis() - StartTime;
+            UpdateTime = TimeBuff + MillisecondTime;
+            Seconds = (int) (UpdateTime / 1000);
+            Minutes = Seconds / 60;
+            Seconds = Seconds % 60;
+
+            textView.setText("" + Minutes + ":"
+                + String.format("%02d", Seconds));
+
+            handler.postDelayed(this, 0);
+        }
+    };
+}
